270633baca35a975be939e786a564c133cb51ebb
hesterhql <49950964+hesterhql@users.noreply.github.com>
2019-10-07 11:48:26 -0400

    Update Triangle.java
    add the second line in github

--- src/test1/Triangle.java
+++ src/test1/Triangle.java
@@ -7,6 +7,7 @@ public class Triangle implements Shape{
 		// TODO Auto-generated method stub
 		System.out.println("Triangle was drawing.");
 		System.out.println("new line");
+		System.out.println("second line ");
 	}
 
 }
